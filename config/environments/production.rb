b5c010d9e8ff3da3934b4d93fd36f6681153679b
Matulg47 <matu47@hotmail.com>
2023-09-13 16:02:24 -0300

    .

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,7 +4,7 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
  
  #config.hosts << "ulahua.up.railway.app"
- config.hosts << "cv7jz10p-3000.brs.devtunnels.ms"
+ #config.hosts << "cv7jz10p-3000.brs.devtunnels.ms"
 
  
   # Code is not reloaded between requests.
@@ -26,7 +26,7 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.public_file_server.enabled = ENV["RAILS_SERVE_STATIC_FILES"].present? || ENV['RENDER'].present?
+  config.public_file_server.enabled = ENV["RAILS_SERVE_STATIC_FILES"].present? 
 
   # Compress CSS using a preprocessor.
   # config.assets.css_compressor = :sass

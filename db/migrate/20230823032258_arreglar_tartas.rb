72f0aa91853809117bf0325f7824a6b953295531
Matulg47 <matu47@hotmail.com>
2023-09-13 13:41:33 -0300

    actualizacion para render

--- db/migrate/20230823032258_arreglar_tartas.rb
+++ db/migrate/20230823032258_arreglar_tartas.rb
@@ -1,8 +1,8 @@
 class ArreglarTartas < ActiveRecord::Migration[7.0]
   def change
-    change_column :tarts,:precio,:integer
-    change_column :tarts,:cantidad,:integer
-    change_column :tarts,:pedido_id,:integer
+    change_column :tarts, :precio, 'integer USING CAST(precio AS integer)'
+    change_column :tarts, :cantidad, 'integer USING CAST(cantidad AS integer)'
+    change_column :tarts,:pedido_id,'integer USING CAST(pedido_id AS integer)'
     remove_column :tarts,:integer
  
   end
--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2023_08_23_032258) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "cakes", force: :cascade do |t|
     t.string "tamaño"
     t.string "nombre"
@@ -89,22 +92,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_23_032258) do
   end
 
   create_table "telegrams", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "tortas", force: :cascade do |t|
-    t.string "nombre"
-    t.string "descripcion"
-    t.string "tamaño"
-    t.integer "precio"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "whatsapps", force: :cascade do |t|
-    t.string "number"
-    t.text "message"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
